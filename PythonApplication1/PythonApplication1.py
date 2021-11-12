bc2497e24772310032ca080957b4f4e73ee15a18
mark salumets <93312154+Stixatvorez@users.noreply.github.com>
2021-11-12 12:00:26 +0200

    Update PythonApplication1.py

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,38 +1,45 @@
-from keyboard import *
-from math import *
-from random import *
+from module1 import *
 while True:
     print("Funktsioonid".center(50,"+"))
-    print("arithmetic- A,\nis_year_leap-Y,\nsquare-S \nseason-Z \nbank-B \nis_prime-I \ndate-D \nXOR_cipher-X \nXOR_uncipher-X2 ")
-    v=input()
+    v=input("arithmetic- A \nis_year_leap- B \nsquare- C \nseason- D \nbank- E \nis_prime- F \ndate- G \nxor_cipher- K")
     if v.upper()=="A":
-        arv1=float(input("Arv 1:"))
-        arv2=float(input("Arv 2:"))
-        sym=input("Tehe:")
-        rezult=arithmetic(arv1,arv2,sym)
-        print(rezult)
-    elif v.upper()=="Y":
-        rezult=is_year_leap(int(input("Sisesta aasta: ")))
-        print(rezult)
-    elif v.upper()=="S":
-        rezult=square(int(input("Sisesta piirkond: ")))
-        print(rezult)
-    elif v.upper()=="Z":
-        rezult=season(int(input("Sisestage kuupäev: ")))
-        print(rezult)
-
+        a=float(input("Arv 1: "))
+        b=float(input("Arv 2: "))
+        c=input("Tehe: ")
+        result=arithmetic(a,b,c)
+        print(result)
     elif v.upper()=="B":
-        rezult=bank(int(input("Kui palju raha soovite laenata: ")))
-        print(rezult)
-    elif v.upper()=="I":
-        rezult=is_prime(int(input("Sisesta ARG  : ")))
-        print(rezult)
+        year=int(input("Введите год: "))
+        result=is_year_leap(year)
+        print(result)
+    elif v.upper()=="C":
+        kv=int(input("Введите сторону квадрата: "))
+        result=square(kv)
+        print(result)
     elif v.upper()=="D":
-        rezult=date(int(input("Sisesta kuupäev: ")))
-        print(rezult)
-    elif v.upper()=="X":
-        rezult=XOR_cipher(int(input("Sisesta F: ")))
-        print(rezult)
-    elif v.upper()=="X2":
-        rezult=XOR_uncipher(int(input("Sisesta F2: ")))
-        print(rezult)
+        kuu=int(input("Введите номер месяца: "))
+        result=season(kuu)
+        print(result)
+    elif v.upper()=="E":
+        a=float(input("Введите сумму депозита: "))
+        years=int(input("Сколько лет прошло?"))
+        result=bank(a,years)
+        print(result)
+    elif v.upper()=="F":
+        a=int(input("Введите номер: "))
+        result=is_prime(a)
+        print(result)
+    elif v.upper()=="K":
+        print("Kodeerimine".center(60,"*"))
+        result=xor_cipher(input("Введите текст"), input("Введите ключ :"))
+        print(result)
+        print("Dekodeerimine". center(60,"*"))
+        de_rezult=xor_uncipher(result, input("Введите ключ:"))
+        print(de_rezult)
+    elif v.upper()=="G":
+        day=int(input("Введите день: "))
+        month=int(input("Введите месяц: "))
+        year=int(input("Введите год: "))
+        result=date(day,month,year)
+        print(result)
+
